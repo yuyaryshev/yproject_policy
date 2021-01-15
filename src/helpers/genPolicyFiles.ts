64e6393b4df21cdc387873243c7ebc906a9f91ca
Infernet <infernet19960406ss@yandex.ru>
2021-01-16 02:12:16 +0300

    update README.md

--- src/helpers/genPolicyFiles.ts
+++ src/helpers/genPolicyFiles.ts
@@ -2,10 +2,10 @@ import { FileMap, PolicyData, PolicyOptions, ProjectData } from "../types";
 
 export function genPolicyFiles(policyData: PolicyData, projectData: ProjectData): FileMap {
     const generateFiles: FileMap = new Map<string, string>();
-    const policyOptions: PolicyOptions = { ...policyData.policy.options, ...projectData.policyConf?.options };
+    const policyOptions: PolicyOptions | undefined = projectData.policyConf?.options;
 
     for (let [relPath, { generate }] of policyData.genFiles.entries()) {
-        generateFiles.set(relPath.replace(/\.gen\.cjs$/, ""), generate(projectData.packageJson ? projectData.packageJson : {}, policyOptions));
+        generateFiles.set(relPath.replace(/\.gen\.cjs$/, ""), generate(projectData.packageJson ?? {}, policyOptions ?? {}));
     }
 
     return generateFiles;
